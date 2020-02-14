d1ecd3aebb10cce2cf5a0025f07304bc588b1498
Chloe Gell <gellert.chloe@gmail.com>
2020-02-14 02:42:54 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -7,10 +7,6 @@ module Players
         "8"
       elsif board.taken?(1) && board.position(1) != self.token && board.taken?(9) && board.position(9) != self.token && !board.taken?(5)
         "5"
-      elsif board.taken?(7) && board.position(7) != self.token && board.taken?(9) && board.position(9) != self.token && !board.taken?(8)
-        "8"
-      elsif board.taken?(7) && board.position(7) != self.token && board.taken?(9) && board.position(9) != self.token && !board.taken?(8)
-        "8"
       elsif board.position(3) == self.token && board.position(9) == self.token && !board.taken?(6)
         "6"
       elsif board.position(7) == self.token && board.position(9) == self.token && !board.taken?(8)
@@ -21,29 +17,29 @@ module Players
         "5" 
       elsif board.position(3) == self.token && board.position(7) == self.token && !board.taken?(5)
         "5"
+      elsif board.taken?(1) && board.taken?(7) && !board.taken?(4)
+        "4"
+      elsif !board.taken?(8)
+        "8" 
+      elsif board.taken?(1) && board.taken?(3) && !board.taken?(2)
+        "2"
+      elsif board.taken?(1) && board.taken?(9) && !board.taken?(5)
+        "5" 
+      elsif board.taken?(3) && board.taken?(7) && !board.taken?(5)
+        "5"
+      elsif board.taken?(3) && board.taken?(9) && !board.taken?(6)
+        "6"
+      elsif board.taken?(7) && board.taken?(9) && !board.taken?(8)
+        "8"
       elsif !board.taken?(1)
         "1" 
       elsif !board.taken?(3)
         "3"
       elsif !board.taken?(7)
         "7" 
-      elsif board.taken?(1) && board.taken?(7) && !board.taken?(4)
-        "4"
       elsif !board.taken?(9)
         "9"
       elsif !board.taken?(5)
-        "5"
-      elsif !board.taken?(8)
-        "8" 
-      elsif board.taken?(3) && board.taken?(9) && !board.taken?(6)
-        "6"
-      elsif board.taken?(7) && board.taken?(9) && !board.taken?(8)
-        "8"
-      elsif board.taken?(1) && board.taken?(3) && !board.taken?(2)
-        "2"
-      elsif board.taken?(1) && board.taken?(9) && !board.taken?(5)
-        "5" 
-      elsif board.taken?(3) && board.taken?(7) && !board.taken?(5)
         "5"
       elsif !board.taken?(2)
         "2"
