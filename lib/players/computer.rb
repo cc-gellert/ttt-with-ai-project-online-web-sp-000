9b6737662d115a08a9f725502283ce2c2341935c
Chloe Gell <gellert.chloe@gmail.com>
2020-02-14 02:49:20 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -38,7 +38,7 @@ module Players
       # elsif board.taken?(3) && board.taken?(9) && !board.taken?(6)
       #   "6"
       # elsif board.taken?(7) && board.taken?(9) && !board.taken?(8)
-        "8"
+        #"8"
       elsif !board.taken?(1)
         "1" 
       elsif !board.taken?(3)
